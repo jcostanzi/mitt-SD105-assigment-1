813f9e80b21ab76479797436cf4410356543f7ce
Javier Costanzi <jcostanzi@student.mitt.ca>
2020-12-17 12:34:30 -0600

    Add stops fetch. Integrate.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const winnipegTransitApiKey = 'X3DdbTvDFRgZYJp__fkc';
 const winnipegTransitApiBaseUrl = 'https://api.winnipegtransit.com/v3';
 const streetsApiUrl = '/streets.json';
+const stopsApiUrl = '/stops.json';
 
 const searchElement = document.querySelector('form input');
 const streetsElement = document.querySelector('.streets');
@@ -13,7 +14,23 @@ class UI {
       const linkElement = document.createElement("a");
       linkElement.innerHTML = `<a href="#" data-street-key="${street.key}">${street.name}</a>`;
 
+      UI.addStreetLinkEventListener(linkElement);
+
       streetsElement.appendChild(linkElement);
+    });
+  }
+
+  static generateStopRows(stops) {
+    stops.forEach(stop => {
+      const rowElement = document.createElement("tr");
+
+      rowElement.innerHTML = `<td>${stop.street.name}</td>
+                              <td>${stop['cross-street'].name}</td>
+                              <td>${stop.direction}</td>
+                              <td></td>
+                              <td></td>`;
+
+      stopsElement.insertAdjacentElement('afterbegin', rowElement);
     });
   }
 
@@ -28,10 +45,35 @@ class UI {
     });
   }
 
-  static cleanDataContainers() {
+  static addStreetLinkEventListener(linkElement) {
+    linkElement.addEventListener('click', event => {
+      const streetKey = event.target.dataset.streetKey;
+
+      UI.cleanStopsTable();
+
+      fetch(`${winnipegTransitApiBaseUrl}${stopsApiUrl}?street=${streetKey}&api-key=${winnipegTransitApiKey}`)
+        .then(response => {
+          console.log(response);
+          if (response.status == 200) {
+            return response.json();
+          } else {
+            Promise.reject(response.statusText);
+          }
+        })
+        .then(data => {
+          UI.generateStopRows(data.stops);
+        });
+    });
+  }
+
+  static cleanStopsTable() {
     displayStreetName.textContent = '';
-    streetsElement.innerHTML = '';
     stopsElement.innerHTML = '';
+  }
+
+  static cleanAllDataContainers() {
+    UI.cleanStopsTable();
+    streetsElement.innerHTML = '';
   }
 }
 
@@ -40,9 +82,9 @@ function search(streetName) {
     return;
   }
 
-  UI.cleanDataContainers();
+  UI.cleanAllDataContainers();
 
-  fetch(`${winnipegTransitApiBaseUrl}${streetsApiUrl}?name=${streetName}&api-key=${winnipegTransitApiKey}`)
+  fetch(`${winnipegTransitApiBaseUrl}${streetsApiUrl}?name=${streetName}&usage=long&api-key=${winnipegTransitApiKey}`)
     .then(response => {
       console.log(response);
       if (response.status == 200) {
@@ -57,11 +99,11 @@ function search(streetName) {
 }
 
 function initialize() {
-  UI.cleanDataContainers();
+  UI.cleanAllDataContainers();
   UI.addSearchEventListener();
 
   // Only for testing porpuses
-  search('portage');
+  search('kenaston');
 }
 
 initialize();
